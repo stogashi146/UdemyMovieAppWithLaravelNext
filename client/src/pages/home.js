254268ef5dae4b533d602d6f8f9c9f8b4578a959
stogashi146 <stogashi146@gmail.com>
2024-03-18 07:54:45 +0900

    Swiperの実装

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,25 @@
 import AppLayout from '@/components/Layouts/AppLayout'
+import axios from 'axios'
 import Head from 'next/head'
+import { useState, useEffect } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 
 const Home = () => {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        const fetchMoviews = async () => {
+            try {
+                const response = await axios.get('api/getPopularMovies')
+                setMovies(response.data.results)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchMoviews()
+    })
+
     return (
         <AppLayout
             header={
@@ -12,7 +30,37 @@ const Home = () => {
             <Head>
                 <title>Laravel - Home</title>
             </Head>
-
+            <Swiper
+                spaceBetween={30}
+                slidesPerView={5}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={swiper => console.log(swiper)}
+                breakpoints={{
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 10,
+                    },
+                    480: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                    },
+                    640: {
+                        slidesPerView: 4,
+                        spaceBetween: 30,
+                    },
+                    768: {
+                        slidesPerView: 5,
+                        spaceBetween: 40,
+                    },
+                }}>
+                {movies.map(movie => (
+                    <SwiperSlide key={movie.id}>
+                        <img
+                            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
